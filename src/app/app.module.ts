f5be67b30174be3ef62ecd5521f386731d1f5700
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-04-23 22:52:19 -1000

    added login, newUser, header component. HTML forms only set up but all are linked to app module. no functionality yet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app/app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { NewUserComponent } from "./pages/newUser/newUser.component";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    NewUserComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
