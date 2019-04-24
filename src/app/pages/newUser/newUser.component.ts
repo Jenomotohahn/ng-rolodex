f404a308e029efe205a4d65d04b0c0d8183944a6
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-04-23 22:59:08 -1000

    added in header/login/newUser components. hooked up but no functionality

---
+++ src/app/pages/newUser/newUser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-newUser",
+  templateUrl: "./newUser.component.html",
+  styleUrls: ["./newUser.component.scss"]
+})
+export class NewUserComponent {
+  constructor() {}
+}
