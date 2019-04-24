19858b9507e0c696efff00159d1369d8cdb632a4
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-04-24 08:18:20 -1000

    added two way binding to the form data in the login component.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,5 +6,12 @@ import { Component } from "@angular/core";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent {
+  loginFormData: {
+    email: string;
+    password: string;
+  } = {
+    email: "",
+    password: ""
+  };
   constructor() {}
 }
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { NgModule } from "@angular/core";
+import { LoginComponent } from "src/app/pages/login/login.component";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"]
+})
+@NgModule({
+  declarations: [LoginComponent]
 })
 export class HeaderComponent {
   user: { username: string } = { username: "" };
